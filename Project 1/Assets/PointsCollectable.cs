cf7d431bcb21c0c8d8e8c6cbbd9b5a171b768517
Diggy12345 <40364032+Diggy12345@users.noreply.github.com>
2018-06-21 11:47:07 -0400

    6-21-18

---
+++ Project 1/Assets/PointsCollectable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsCollectable : MonoBehaviour 
+{
+    public int points;
+    HUD_Manager hudmanager;
+
+    // Use this for initialization
+    void Start() {
+        hudmanager = GameObject.FindGameObjectWithTag("Player").GetComponent<HUD_Manager>();
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            gameObject.SetActive(false);
+            hudmanager.UpdateScore(points);
+        }
+   
+    }
+}
