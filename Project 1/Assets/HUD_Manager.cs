0b70fe71763146ae3693d8aca732bac319a78c0f
Diggy12345 <40364032+Diggy12345@users.noreply.github.com>
2018-06-25 11:42:21 -0400

    6-25-18

--- Project 1/Assets/HUD_Manager.cs
+++ Project 1/Assets/HUD_Manager.cs
@@ -5,20 +5,30 @@ using UnityEngine.UI;
 
 public class HUD_Manager : MonoBehaviour
 {
+    public int StartingHealth;
+    int CurrentHealth;
+
     int score;
     public Text scoretext;
-
+    public Slider healthbar;
     // Use this for initialization
     void Start()
     {
         score = 0;
         scoretext.text = "Score: " + score;
+        CurrentHealth = StartingHealth;
+        healthbar.value = CurrentHealth;
     }
+
 
     public void UpdateScore(int amount)
     {
         score = score + amount;
         scoretext.text = "Score: " + score;
     }
-
+    public void UpdateHealth(int amount)
+    {
+        CurrentHealth = CurrentHealth + amount;
+        healthbar.value = CurrentHealth;
+    }
 }
--- Project 1/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Project 1/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -10,34 +10,43 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
         public Transform target;                                    // target to aim for
-
+        public Animator Anim;
 
         private void Start()
         {
             // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
+            Anim = GetComponent<Animator>();
+            agent.updateRotation = false;
+            agent.updatePosition = true;
 
-	        agent.updateRotation = false;
-	        agent.updatePosition = true;
         }
 
 
         private void Update()
         {
             if (target != null)
+            {
                 agent.SetDestination(target.position);
-
+            }
             if (agent.remainingDistance > agent.stoppingDistance)
+            {
                 character.Move(agent.desiredVelocity, false, false);
+                Anim.SetBool("Attacking", true);
+            }
             else
+            {
                 character.Move(Vector3.zero, false, false);
+                Anim.SetBool("Attacking", true);
+            }
+
         }
 
+            public void SetTarget(Transform target)
 
-        public void SetTarget(Transform target)
-        {
-            this.target = target;
+            {
+                this.target = target;
+            }
         }
     }
-}
