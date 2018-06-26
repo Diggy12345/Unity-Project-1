c861636ba7217dff4a096a561dc9cf59f87dda97
Diggy12345 <40364032+Diggy12345@users.noreply.github.com>
2018-06-26 11:39:57 -0400

    6-26-18

--- Project 1/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Project 1/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -1,10 +1,11 @@
 using System;
 using UnityEngine;
 
+
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
-    [RequireComponent(typeof (ThirdPersonCharacter))]
+    [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
     public class AICharacterControl : MonoBehaviour
     {
         public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
@@ -18,12 +19,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
 
-	        agent.updateRotation = false;
-	        agent.updatePosition = true;
+            agent.updateRotation = false;
+            agent.updatePosition = true;
         }
 
+        
 
-        private void Update()
+    private void Update()
         {
             if (target != null)
                 agent.SetDestination(target.position);
@@ -41,3 +43,4 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         }
     }
 }
+
